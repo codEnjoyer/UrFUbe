e78ea67893bd40b8d308da7b0e1989621bd7635b
Polina <polyabikka@gmail.com>
2023-06-12 01:43:16 +0500

    Update VideoPromo.vue

--- frontend/src/components/VideoPromo.vue
+++ frontend/src/components/VideoPromo.vue
@@ -1,7 +1,13 @@
 <template>
- <div class="container__video">
+ <div  class="container__video">
    <div class="preview__wrap">
-     <img :src="pre" class="preview">
+     <img @click="watch_video" :src="pre" class="preview">
+     <router-link to="/search" @click="watch_video" class="name">
+       <a>{{name}}</a>
+     </router-link>
+     <router-link :to="'/user/' + user_id">
+       <a class="username">{{user_name}}</a>
+     </router-link>
    </div>
  </div>
 </template>
@@ -10,7 +16,10 @@
 export default {
   name: "video-promo",
   props: {
-    preview: { default: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' }
+    preview: { type: String, default: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' },
+    name: { type: String, default: 'Очень крутое видео, посмотрите какие круты е котята, очень классные котята, самое милое видео навсегда' },
+    user_id: { type: Number, default: 0 },
+    user_name: { type: String, default: "aynjysadasdf sdasdasdasdasdasddddddddddddddsdf ddddddddasddddddd dddddddddddddd"}
   },
   data() {
     return {
@@ -19,6 +28,11 @@ export default {
   },
   mounted() {
     this.pre = this.preview;
+  },
+  methods: {
+    watch_video() {
+      this.$router.push('/search');
+    }
   }
 }
 </script>
@@ -45,5 +59,29 @@ export default {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+.name {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 18px;
+  text-transform: uppercase;
+  text-align: inherit;
+  margin-bottom: 0;
+}
+.username {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 18px;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
