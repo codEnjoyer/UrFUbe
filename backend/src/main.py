481010fccfd4b7efc357d509330520a7b4c10bc4
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-06-11 17:06:30 +0500

    Добавил сервис фронтенда в docker-compose.yaml

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import HTMLResponse
+# from fastapi.middleware.cors import CORSMiddleware
 
 from auth.base_config import fastapi_users, auth_backend, current_user
 from auth.models import User
@@ -8,8 +9,18 @@ from auth.schemas import UserRead, UserCreate
 from tasks.router import router as router_tasks
 from auth.manager import get_user_by_id
 from videos.router import router as video_router, get_presigned_url, get_user_video_models
+
+from config import FRONT_APP_PORT
+
 app = FastAPI(title='First FastAPI app')
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[f"http://localhost:{FRONT_APP_PORT}"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
+// import axios from 'axios';
 import App from './App'
 import router from '@/router/router.js'
 
@@ -7,5 +8,8 @@ const app = createApp(App)
 app
     .use(router)
     .mount('#app')
+
+// axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = 'http://localhost:${BACK_PORT}/';
 
 import "bootstrap/dist/js/bootstrap.js"
