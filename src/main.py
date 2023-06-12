b5adf8de760631fddfaed01bd6633095d94f3fcb
Tupi8mpl <i.tupikov12@gmail.com>
2023-06-12 19:02:34 +0500

    Add upload large files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import HTMLResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_async_session
 
 from auth.base_config import fastapi_users, auth_backend, current_user
 from auth.models import User
@@ -8,8 +11,24 @@ from auth.schemas import UserRead, UserCreate
 from tasks.router import router as router_tasks
 from auth.manager import get_user_by_id
 from videos.router import router as video_router, get_presigned_url, get_user_video_models
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title='First FastAPI app')
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -34,20 +53,3 @@ def protected_route(user: User = Depends(current_user)):
 @app.get("/unprotected-route")
 def unprotected_route():
     return f"Hello, anonymous!"
-
-
-@app.get("/", response_class=HTMLResponse)
-async def main_window():
-    video = await get_user_video_models(1, 1)
-    url = await get_presigned_url(f"{video.video_url}")
-    return f'''
-    <html>
-        <head>
-            <title> TEST VIDEO SERVICES </title>
-        </head>
-        <body>
-            <h1> ЗДЕСЬ МОГЛА БЫТЬ ВАША РЕКЛАМА </h1>
-            <video width="500" src="{url}" controls></video>
-        </body>
-    </html>
-    '''
