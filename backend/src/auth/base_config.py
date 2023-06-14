ee8b5381d0e7162f563e3519159e1449bdf3b839
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-06-15 01:47:28 +0500

    Изменил докерфайл для деплоя, не работает

--- backend/src/auth/base_config.py
+++ backend/src/auth/base_config.py
@@ -12,7 +12,7 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET_KEY_JWT, lifetime_seconds=60 * 60)
 
 
-cookie_transport = CookieTransport(cookie_name="bonds", cookie_max_age=60 * 60)
+cookie_transport = CookieTransport(cookie_name="untasty_and_tochka", cookie_max_age=60 * 60)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
